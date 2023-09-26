99f90d556b0ce81fd259dc0cbfb73e81e6f0e01e
Galisteoo00 <115107104+Galisteoo00@users.noreply.github.com>
2023-09-26 09:18:43 +0200

    Delete ud2dwes directory

--- ud2dwes/ej4.php
+++
@@ -1,7 +0,0 @@
-<?php
-$ciclo="DAW";
-$modulo="DWES";
-print "<p>";
-printf("%s es un módulo de %d curso de %s", $modulo, 2, $ciclo);
-print "</p>";
-?>
--- ud2dwes/ej5.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$a = 3;
-$b = 5;
-
-echo "La suma total es " . ($a + $b);
-?>
